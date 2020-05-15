b3bdca35560b0a663246da596378e7ee94dd07b0
Aram Boyajyan <dev@aram.cz>
2020-05-15 10:17:36 +0200

    adding react-icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { FaHome } from 'react-icons/fa';
+
 import Layout from "../components/index"
 import SEO from "../components/other/seo"
 import Container from "../components/layout/container";
@@ -9,7 +11,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Container>
       <h1>Home</h1>
-      <p>Placeholder</p>
+      <p><FaHome /> Placeholder</p>
     </Container>
   </Layout>
 )
